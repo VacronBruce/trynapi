f8133070499ecbaaf72cd9fb31948ccf23832eb3
Bruce <homan311@gmail.com>
2024-02-07 08:47:08 -0500

    Register again after tryCb

--- trycb/src/lib.rs
+++ trycb/src/lib.rs
@@ -1,13 +1,13 @@
 #![deny(clippy::all)]
 
-use napi::{Env, JsFunction, JsObject, Ref, Result};
+use napi::{ threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode}, Env, JsFunction, JsObject, Ref, Result};
 
 #[macro_use]
 extern crate napi_derive;
 
 struct CallbackContext {
-  callback: Ref<()>
-} 
+  callback: Ref<()>,
+}
 
 #[napi]
 struct SimpleCall {
@@ -19,28 +19,36 @@ struct SimpleCall {
 impl SimpleCall {
   #[napi(constructor)]
   pub fn new() -> Self {
-    SimpleCall { cb:  None, env: None}
+    SimpleCall {
+      cb: None,
+      env: None,
+    }
   }
 
   #[napi]
   pub fn register_cb(&mut self, env: Env, cb: JsFunction) -> Result<()> {
     let mut js_obj = env.create_object()?;
-    let js_fn_ref = env.create_reference(cb).unwrap();
+    let js_fn_ref = env.create_reference_with_refcount(cb, 10).unwrap();
+    
     let ctx = CallbackContext {
       callback: js_fn_ref,
-    }; 
+    };
+    
     env.wrap(&mut js_obj, ctx)?;
+    
     self.cb = Some(js_obj);
     self.env = Some(env);
     Ok(())
   }
 
   #[napi]
-  pub fn try_cb(&self, env: Env) -> Result<()> {
-    if let Some(js_obj) = &self.cb {      
-      let ctx: &mut CallbackContext = env.unwrap(&js_obj)?;
-      let js_fn: JsFunction = env.get_reference_value(&ctx.callback)?;
-      let _ = js_fn.call_without_args(None);
+  pub fn try_cb(&mut self) -> Result<()> {
+    if let Some(env) = &mut self.env {
+      if let Some(js_obj) = &self.cb {
+        let ctx: &mut CallbackContext = env.unwrap(&js_obj)?;
+        let js_fn: JsFunction = env.get_reference_value(&ctx.callback)?;
+        let _ = js_fn.call_without_args(None);
+      }
     }
     Ok(())
   }
--- trycb/test.mjs
+++ trycb/test.mjs
@@ -4,4 +4,28 @@ const simple = new SimpleCall();
 const fp = () => { console.log("Rust call js function");}
 simple.registerCb(fp);
 simple.tryCb();
-simple.tryCb(); //Second time execution will pop up error
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+simple.registerCb(fp);
+simple.tryCb();
+setTimeout(() => {
+    console.log("Timeout ");
+    simple.registerCb(fp);
+    simple.tryCb(); //Second time execution will pop up error
+}, 2000);
